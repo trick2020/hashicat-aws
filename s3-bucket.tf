4b26ddf58ce22392a4a309c7b80d6009d7f54ae2
rickt <t2rick@yahoo.com>
2021-01-13 19:38:36 +0000

    Added s3 bucket module to my Terraform. Run #4.

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,10 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/RICKTANG-trainging/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-  bucket_prefix = "rt-"
-  tags = {
-      Owner = "Gaurav"
-      Department = "devops"
-      Billable = "yes"
-  }
+  bucket_prefix = "${var.prefix}"
 }
